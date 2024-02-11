9d18d93659290198ca04ec7c21909850a82b3654
R M Shahidul Islam <shahedbddev@gmail.com>
2024-02-11 18:39:19 +0600

    + Project code added

---
+++ ExtensionMethodsEx/Program.cs
@@ -0,0 +1,23 @@
+﻿using ExtensionMethodsEx;
+
+string input = "hello, world! sudo apt install dotnet-sdk-8.0";
+string titleCase = input.ToTitleCaseNew();
+Console.WriteLine(titleCase);  // Output: Hello, World! Sudo Apt Install Dotnet-Sdk-8.0
+
+
+List<Person> people = new List<Person>
+            {
+                new Person { Id = 1, Name = "John" },
+                new Person { Id = 2, Name = "Jane" },
+                new Person { Id = 1, Name = "John" }, // Duplicate Id
+                new Person { Id = 3, Name = "Alice" }
+            };
+
+IEnumerable<Person> distinctPeople = people.DistinctByCustom(p => p.Id);
+foreach (Person person in distinctPeople)
+{
+    Console.WriteLine($"Id: {person.Id}, Name: {person.Name}");
+}
+
+
+
---
+++ ExtensionMethodsEx/Service.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace ExtensionMethodsEx
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> DistinctByCustom<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (T element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+        public static string ToTitleCase(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return str;
+
+            return char.ToUpper(str[0]) + str.Substring(1).ToLower();
+        }
+
+        public static string ToTitleCaseNew(this string s) 
+            => CultureInfo.InvariantCulture.TextInfo.ToTitleCase(s.ToLower());
+    }
+}
